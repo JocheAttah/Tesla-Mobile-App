1b7bb627d9964bdf344d1e087ca6480a478f5d76
Attah David <jocheattahdavid@gmail.com>
2021-01-14 10:49:17 +0100

    completed buttons

--- components/CarItem/styles.js
+++ components/CarItem/styles.js
@@ -12,7 +12,7 @@ const styles = StyleSheet.create({
       },
       title: {
         fontSize: 40,
-        fontWeight: "600",
+        fontWeight: "bold",  // 600
       },
     
       subtitle: {
