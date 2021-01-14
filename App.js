90c73617a69fba9eef16e3b35698827748e97c55
Attah David <jocheattahdavid@gmail.com>
2021-01-14 14:50:33 +0100

    added Header

--- App.js
+++ App.js
@@ -2,10 +2,12 @@ import React from "react";
 import { StyleSheet, View } from "react-native";
 // import CarItem from "./components/CarItem";
 import CarList from "./components/CarList";
+import Header from "./components/Header";
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Header />       
       <CarList />
     </View>
   );
